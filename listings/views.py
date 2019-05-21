3c74b0c138e9782117a5daa63dfa77961178f42f
RIZVI Faizan Ext OBS/S APAC <vndb2232@orange.com>
2019-05-21 17:06:25 +0530

    First commit

---
+++ listings/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger,Paginator
+from listings.choices import price_choices,bedroom_choices,state_choices
+
+# Create your views here.
+def index(request):
+	listings=Listing.objects.all()
+	paginator= Paginator(listings,2)
+	page= request.GET.get('page')
+	paged_listings= paginator.get_page(page)
+
+
+	context= {'listings':paged_listings}
+	return render(request, "listings/listings.html", context)
+
+def listing(request, listing_id ):
+	listing= get_object_or_404(Listing,pk=listing_id)
+	context={
+		'listing': listing
+	}
+	return render(request, "listings/listing.html",context)
+
+def search(request):
+
+	queryset_list= Listing.objects.order_by('-list_date')
+
+	#keywords
+	if 'keywords' in request.GET:
+
+		keywords= request.GET["keywords"]
+
+		if keywords:
+			queryset_list=queryset_list.filter(description__icontains=keywords)
+
+	# city
+	if 'city' in request.GET:
+		city = request.GET["city"]
+
+		if city:
+			queryset_list = queryset_list.filter(city__iexact=city)
+
+	# state
+	if 'state' in request.GET:
+		state = request.GET["state"]
+
+		if state:
+			queryset_list = queryset_list.filter(state__iexact=state)
+
+
+
+
+
+
+
+
+	context={
+
+		'state_choices': state_choices,
+		'bedroom_choices': bedroom_choices,
+		'price_choices': price_choices,
+		'listings':queryset_list,
+		'values': request.GET
+	}
+	
+	return render(request, "listings/search.html",context)
